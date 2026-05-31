39130303dcff19dcb269e4c9908ff73d9d383410
SuccessForce Deploy <deploy@successforce.agency>
2026-05-31 18:54:54 +0500

    Fix migration to use new Field()

--- pb_migrations/1715000000_add_user_fields.js
+++ pb_migrations/1715000000_add_user_fields.js
@@ -1,19 +1,19 @@
 migrate((app) => {
   const collection = app.findCollectionByNameOrId("users");
   
-  collection.fields.add({
+  collection.fields.add(new Field({
     name: "role",
     type: "text",
     required: true
-  });
-  collection.fields.add({
+  }));
+  collection.fields.add(new Field({
     name: "display_name",
     type: "text"
-  });
-  collection.fields.add({
+  }));
+  collection.fields.add(new Field({
     name: "must_change_password",
     type: "bool"
-  });
+  }));
   
   app.save(collection);
 }, (app) => {
